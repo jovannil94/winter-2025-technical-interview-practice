1d5bc78f8a75cccc7f3d857230dd34d95a62d30a
jovannil94 <jovannil94@gmail.com>
2025-01-27 21:59:09 -0500

    Completing lesson 2

--- lesson02-strings/problem.js
+++ lesson02-strings/problem.js
@@ -5,7 +5,21 @@
  * @return {number}
  */
 function lengthOfLongestSubstring(s) {
-  // Your code here
+  if(s.length <= 1) return s.length;
+  let maxLength = 0;
+  let start = 0;
+  let uniqueLetters = new Set();
+
+  for(let i in s) {
+    while(uniqueLetters.has(s[i])) {
+      uniqueLetters.delete(s[start]);
+      start++;
+    }
+    uniqueLetters.add(s[i]);
+    maxLength = Math.max(maxLength, uniqueLetters.size);
+  }
+
+  return maxLength;
 }
 
 module.exports = lengthOfLongestSubstring;
--- lesson02-strings/warmup.js
+++ lesson02-strings/warmup.js
@@ -1,17 +1,48 @@
 // 1. given a string with three characters, return the second character
 
+const returnSecondChar = (str) => {
+    return str[1];
+};
+
+console.log(returnSecondChar("cat"))
+//expect 'a' to be returned
+
+test('return second character', () => {
+    expect(returnSecondChar('cat')).toBe('a');
+});
+
 // 2. get the second and third char from a string. the string will always have atleast three characters
+
+returnSecondThirdChar = (str) => {
+    return str[1] + str[2];
+};
+
+console.log(returnSecondThirdChar('cat'));
+console.log(returnSecondThirdChar('robbed'));
 
 // 'cat' => 'at'
 // 'robbed' => 'ob'
 
 // 3. Given a word with 5 letters, return the second through 5th letter
 
+returnSecondToFifth = (str) => {
+    return str.slice(1);
+};
+
+console.log(returnSecondToFifth('Hello'));
+
 // function getSecondThroughFifthLetter (word){
 //   return word.slice(1);
 // }
 
 // 4. given a string and a number called N, return the second through the Nth character
+
+const returnSecondToNth = (str, n) => {
+    return str.slice(1, n);
+};
+
+console.log(returnSecondToNth('schoolbus', 5));
+console.log(returnSecondToNth('telephone', 6));
 
 // 'schoolbus', 5 => 'choo'
 // 'telephone', 6 => 'eleph
@@ -21,9 +52,34 @@
 
 // 5. Given an array of booleans, return an arrray of only false elements
 
+const returnFalseEle = (arr) => {
+    let results = [];
+
+    for(let i in arr) {
+        if(arr[i] === false){
+            results.push(false);
+        }
+    }
+    return results;
+};
+
+console.log(returnFalseEle([true, true, false, true , false]));
+
 // [true, true, false, true , false] => [false, false]
 
 // 6. given a string with two words, put ' the ' between the words and return the string
+
+const insertThe = (str) => {
+    let arr = str.split(' ');
+    arr.splice(1,0,'the');
+
+    return arr.join(' ');
+};
+
+console.log(insertThe('to beach'));
+console.log(insertThe('for real'));
+console.log(insertThe('jim john'));
+console.log(insertThe('to house'));
 
 // 'to beach' => 'to the beach'
 // 'for real' => 'for the real'
@@ -31,6 +87,14 @@
 // 'to house' => 'to the house'
 
 // 7.  given two numbers, price and discount, return the price after the discount is applied
+
+const applyDiscount = (price, discount) => {
+    let priceOff = (discount/100) * price;
+    return price - priceOff;
+};
+
+console.log(applyDiscount(100, 15));
+console.log(applyDiscount(70, 3));
 
 // price is 100 and discount is 15
 
